c1d98e907d0bc811cdd0bebe1a70339c599fc161
Kalpam Vinay <kalpam.vinay@ameexusa.net>
2021-03-26 21:33:49 +0530

    css added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import "./App.css";
 const products = [
-  { id: "1", title: "Mobile", price: "$199", Url: "./mobile.jpg" },
-  { id: "2", title: "Laptop", price: "$499", Url: "./Laptop.jpg" },
-  { id: "3", title: "Tablet", price: "$249", Url: "./Ipad.jpg" }]
+  { id: "1", title: "Iphone11", price: "$199",description: "Iphone11 comes with IOS and has triple camera setup experience the best picture quality ", Url: "./mobile.jpg" },
+  { id: "2", title: "Hp Laptop", price: "$499",description: "This laptop has the latest intel core and is fast has touch screen with good battery backup", Url: "./Laptop.jpg" },
+  { id: "3", title: "Ipad", price: "$249",description: "Ipad comes with IOS and  has big screen that supports 4k and hdr modes", Url: "./Ipad.jpg" }]
 class App extends React.Component {
   render() {
     return (
@@ -17,7 +17,8 @@ class App extends React.Component {
           <div className="cards">
             <img src={items.Url} alt="img" />
             <h1>{items.title}</h1>
-            <p>Price:{items.price}</p>
+            <h2>Price:<span class="price">{items.price}</span></h2>
+            <p>{items.description}</p>
             <button class="add-to-cart">Add to Cart</button>
           </div>
         </div>
